dde14843b8f5d9a47e9551b11bc80297225af12b
Richard Preston <richard.preston@tufts.edu>
2019-02-06 13:36:31 -0500

    finished video functionality

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,32 @@ $('input[name="mediaOptions"]').change(function () {
   $('.media-container').addClass('d-none')
   $('#' + $(this).attr('value')).addClass('fade-in-f').removeClass('d-none')
 })
+
+// YouTube videos
+$('.youtube').each(function () {
+  const div = $(this)
+  let parent = div
+  if (div.find('div.play-button').length == 0) {
+    const link = $('<a />')
+      .attr('href', 'https://youtu.be/' + div.data('embed'))
+      .attr('target', '_blank')
+    div.append(link)
+    parent = link
+  }
+  const img = $('<img />')
+    .attr('src',  'https://img.youtube.com/vi/' + div.data('embed') + '/hqdefault.jpg')
+    .on('load', function () {
+      parent.append(img)
+    })
+}).click(function () {
+  const div = $(this)
+  if (div.find('div.play-button').length > 0) {
+    const iframe = $('<iframe />')
+      .attr('frameborder', '0')
+      .attr('allow', 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture')
+      .attr('allowfullscreen', '')
+      .attr('src', 'https://www.youtube-nocookie.com/embed/' + div.data('embed') + '?autoplay=1')
+    div.empty()
+    div.append(iframe)
+  }
+})
